8212157d3bc46198b6325fb31b4b1517b3f3e2be
Ryan Deng <ryan.deng@berkeley.edu>
2020-01-22 22:35:32 -0800

    Add in question 3

---
+++ src/sql_script.sql
@@ -0,0 +1,7 @@
+-- Create tables
+create table Product(name varchar(100) PRIMARY KEY, category varchar(100) NOT NULL);
+create table PriceHistory(name varchar(100) NOT NULL, price integer NOT NULL, start_date DATE NOT NULL, end_date DATE,
+            FOREIGN KEY (name) REFERENCES Product(name));
+
+-- Get current price
+select product.name, history.price from Product as product, PriceHistory as history where product.name = history.name AND history.end_date IS NOT NULL;
