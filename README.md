6180a09fddc7975b3509ded59ebcb10a4b69160d
Ryan Deng <ryan.deng@berkeley.edu>
2020-01-22 22:42:11 -0800

    Update

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Taboola Take-Home Assignment
 
 - Taboola.java has the two questions for the take-home assignment.
 - TaboolaTest.java is a series of tests to test the functionality of the two questions.
+- sql_script.sql contains the script that creates the two tables and has a query which joins the two tables to get the current price
--- sql_script.sql
+++ sql_script.sql
@@ -3,5 +3,5 @@ create table Product(name varchar(100) PRIMARY KEY, category varchar(100) NOT NU
 create table PriceHistory(name varchar(100) NOT NULL, price integer NOT NULL, start_date DATE NOT NULL, end_date DATE,
             FOREIGN KEY (name) REFERENCES Product(name));
 
--- Get current price
-select product.name, history.price from Product as product, PriceHistory as history where product.name = history.name AND history.end_date IS NOT NULL;
+-- Get current price of all items
+select product.name, history.price as current_price from Product as product, PriceHistory as history where product.name = history.name AND history.end_date IS NOT NULL;
